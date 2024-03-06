6da7965dce05aef38b26d105c848bf369b2ad921
Jacob Dimoff <jacobdimoff@gmail.com>
2024-03-06 14:22:46 -0400

    added coords. Need to change the location

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import StadiaMaps from 'ol/source/StadiaMaps.js';
 
 const map = new Map({
   target: 'map',
@@ -15,3 +17,14 @@ const map = new Map({
     zoom: 2
   })
 });
+
+const mousePositionControl = new MousePosition({
+	coordinateFormat: createStringXY(2),
+	projection: 'EPSG:4326',
+	// comment the following two lines to have the mouse position
+	// be placed within the map.
+	// className: 'custom-mouse-position',
+	target: document.getElementById('mouse-position'),
+  });
+
+map.addControl(mousePositionControl)
